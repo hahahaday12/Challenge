8ce5ce280dd1f9668dc82640ff804b59b52582b1
haeun_Kim <hahahaday12@naver.com>
2022-07-26 23:30:39 +0900

    5.ReactStudy

--- ex1/src/App.js
+++ ex1/src/App.js
@@ -1,8 +1,21 @@
-import Say from './say';
-import './App.css';
+import UserNumber from './day05/map';
+import Main from './day05/map';
+import'./App.css';
 
-const App = () => {
-  return <Say / >;
+
+const App  = () => {
+  const names = [
+        {fruite: "자두", price:2000}, 
+        {fruite: "바나나", price:1500},
+        {fruite:"사과", price:2500}
+  ]
+  const nameList = names.map( (v) => (<Main name={v.fruite} price={v.price}/>))
+  return(
+      <div>
+            {nameList}
+      </div>
+  )
 };
+
 
 export default App;
